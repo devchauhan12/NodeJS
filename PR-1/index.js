33d9b3fe17a349bd04483af47696f935d737da4f
Dev chauhan <devchauhansurat@gmail.com>
2024-03-04 02:41:13 +0530

    NodeJS

---
+++ PR-1/index.js
@@ -0,0 +1,28 @@
+const http = require('http')
+// const url = require('url')
+const fs = require('fs')
+
+http.createServer((req, res) => {
+    // let pathname = url.parse(req.url).pathname;
+    if (req.url == "/") {
+        res.end('Home Page')
+    } else if (req.url == '/about') {
+        fs.readFile('about.txt', 'utf-8', (err, data) => {
+            console.log(req.url)
+            // console.log(data)
+            res.end(data);
+        });
+    } else if (req.url == '/product') {
+        fs.readFile('product.txt', 'utf-8', (err,data) => {
+            res.end(data);
+        });
+    } else if (req.url == '/contact') {
+        fs.readFile('contact.txt', 'utf-8', (err,data) => {
+            res.end(data);
+        });
+    } else {
+        res.write('Error 404!')
+    }
+    res.end()
+}).listen(8000)
+console.log("server is running at 8000")
